f6555ff71455d8f4b79bdaf920fa1c9c73e214d3
QuUsman <61276867+QuUsman@users.noreply.github.com>
2020-05-03 19:33:09 +0300

    Program Just started

---
+++ CountingSort/CountingSort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+
+int main(void) 
+{
+	
+	int arr[30] = {2, 2, 1, 0, 1, 0, 1, 1, 0, 2, 2, 1, 1, 1, 2, 0, 0, 1, 1, 0, 1, 0, 1, 2, 0, 0, 0,1, 1, 2};
+	
+	int arrayLength = sizeof(arr) / sizeof(arr[0]);   // Array size is already given, just to make our program generic
+	
+	return 0;
+} 
