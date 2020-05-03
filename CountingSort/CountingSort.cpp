59d1e3861d3637d94aadff73f403a99e1221447d
QuUsman <61276867+QuUsman@users.noreply.github.com>
2020-05-03 20:01:18 +0300

    Program at intermediate level

--- CountingSort/CountingSort.cpp
+++ CountingSort/CountingSort.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
+/*
+  Below Program is a solution of Technical Assesment test given by 
+                     Envio System Inc. 
+  
+    Developed by Usman Asghar
 
+  
+  The program is to provide sorting algorithm for given integer array
+  
+  with linear time complexcity in order of O(n). 
+*/
+
+
+/* With all available Sorting algorithms, mostly are Comparison based but
+   Radix sort and Counting sort are O(N). They are NOT comparison-based sorts, 
+   which have been proven to have O(N log N) lower bound.
+
+  To be more accurate, radix sort is O(kN), where k is the number of digits.
+  Counting sort is O(N + k), where k is the range of the numbers to be sorted.
+
+  In particular applications like this, where k is faily small that both Radix sort 
+  and Counting sort exhibit linear-time performance.
+  
+*/
+
+
+// Prototypes of used functions
+int getMaximum(int arr[], int);
+int getMinimum(int arr[], int);
+void countingSort(int arr[], int, int);
 
 
 int main(void) 
@@ -11,3 +40,32 @@ int main(void)
 	
 	return 0;
 } 
+
+void countingSort(int arr[], int length, int range)
+{
+	
+}
+
+int getMaximum(int arr[], int length)
+{
+	int max = arr[0];
+	int i;
+	for (i = 1; i < length; i++)
+		{
+			if (arr[i] > max)
+			max = arr[i];
+		}
+	return max;
+}
+
+int getMinimum(int arr[], int length)
+{
+	int min = arr[0];
+	int i;
+	for (i = 1; i < length; i++)
+		{
+		    if (arr[i] < min)
+			    min = arr[i];
+		}
+	return min;
+}
